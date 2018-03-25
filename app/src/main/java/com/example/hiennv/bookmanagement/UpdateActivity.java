ccab8591aee07b50dc42ec545ad42b2fc38b062f
hiennguyen0412 <nguyenhien4120@gmail.com>
2018-03-25 23:46:48 +0700

    done update book

--- app/src/main/java/com/example/hiennv/bookmanagement/UpdateActivity.java
+++ app/src/main/java/com/example/hiennv/bookmanagement/UpdateActivity.java
@@ -1,20 +1,30 @@
 package com.example.hiennv.bookmanagement;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.hiennv.bookmanagement.utils.DBUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class UpdateActivity extends AppCompatActivity {
     final String DATABASE_NAME = "BookManagement.sqlite";
+    final int REQUEST_CHOOSE_PHOTO = 321;
     EditText edtID, edtName, edtAuthor, edtType, edtPrice;
     ImageView imgUpdate;
     Button btnSave, btnCancel, btnSelect;
@@ -27,6 +37,32 @@ public class UpdateActivity extends AppCompatActivity {
 
         addControls();
         loadToUpdate();
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePhoto();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(update()==1){
+                    Toast.makeText(UpdateActivity.this, "Update OK", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(UpdateActivity.this, "Update failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancel();
+            }
+        });
 
     }
 
@@ -47,6 +83,7 @@ public class UpdateActivity extends AppCompatActivity {
         Bitmap bm = BitmapFactory.decodeByteArray(img, 0, img.length);
         imgUpdate.setImageBitmap(bm);
 
+        edtID.setText(id+"");
         edtName.setText(name);
         edtAuthor.setText(author);
         edtType.setText(type);
@@ -67,4 +104,64 @@ public class UpdateActivity extends AppCompatActivity {
         btnCancel = findViewById(R.id.btnCancel);
 
     }
+
+    private void choosePhoto(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,REQUEST_CHOOSE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQUEST_CHOOSE_PHOTO){
+                Uri imgUri = data.getData();
+                try {
+                    InputStream is = getContentResolver().openInputStream(imgUri);
+                    Bitmap bm = BitmapFactory.decodeStream(is);
+                    imgUpdate.setImageBitmap(bm);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private int update(){
+        int id = Integer.parseInt(edtID.getText().toString());
+        String name = edtName.getText().toString();
+        String author = edtAuthor.getText().toString();
+        String type = edtType.getText().toString();
+        double price = Double.parseDouble(edtPrice.getText().toString());
+
+
+        byte[] img = getByteArrayFromImageView(imgUpdate);
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Name", name);
+        contentValues.put("Author", author);
+        contentValues.put("Type", type);
+        contentValues.put("Price", price);
+        SQLiteDatabase database = DBUtils.initDatabase(this, DATABASE_NAME);
+        database.update("Book", contentValues, "ID = ?", new String[]{id+""});
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        return 1;
+
+
+    }
+     private byte[] getByteArrayFromImageView(ImageView imgv){
+         BitmapDrawable drawable = (BitmapDrawable) imgv.getDrawable();
+         Bitmap bmp = drawable.getBitmap();
+
+         ByteArrayOutputStream stream = new ByteArrayOutputStream();
+         bmp.compress(Bitmap.CompressFormat.PNG, 100,stream);
+         byte[] byteArray = stream.toByteArray();
+         return byteArray;
+     }
+
+     private void cancel(){
+         Intent intent = new Intent(this, HomeActivity.class);
+         startActivity(intent);
+     }
 }
