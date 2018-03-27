9ab79dca50801818d2815523e89f1b5f550fed5e
hiennguyen0412 <nguyenhien4120@gmail.com>
2018-03-27 19:08:45 +0700

    add function delete book

--- app/src/main/java/com/example/hiennv/bookmanagement/AdapterBook.java
+++ app/src/main/java/com/example/hiennv/bookmanagement/AdapterBook.java
@@ -1,10 +1,15 @@
 package com.example.hiennv.bookmanagement;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +18,10 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hiennv.bookmanagement.model.Book;
+import com.example.hiennv.bookmanagement.utils.DBUtils;
 
 import java.util.ArrayList;
 
@@ -72,7 +79,56 @@ public class AdapterBook extends BaseAdapter {
                 context.startActivity(intent);
             }
         });
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(context, "Del????", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(context);
+                }
+                builder.setTitle("Delete book")
+                        .setMessage("Are you sure you want to delete this book?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                                deleteBook(book.getId());
+                                Toast.makeText(context, "Delete successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
         return row;
+
+    }
+
+    private void deleteBook(int idBook) {
+        SQLiteDatabase database = DBUtils.initDatabase(context, "BookManagement.sqlite");
+        database.delete("Book", "ID = ?", new String[]{idBook+""});
+        Cursor cursor = database.rawQuery("SELECT * FROM Book", null);
+        list.clear();
+        for(int i = 0; i <cursor.getCount();i++){
+            cursor.moveToPosition(i);
+            int id = cursor.getInt(0);
+            String name = cursor.getString(1);
+            String author = cursor.getString(2);
+            String type = cursor.getString(3);
+            Double price = cursor.getDouble(4);
+            byte[] image = cursor.getBlob(5);
+            list.add(new Book(id, name, author,type,price,image));
+
+        }
+        notifyDataSetChanged();
     }
 
     @Nullable
@@ -80,6 +136,7 @@ public class AdapterBook extends BaseAdapter {
     public CharSequence[] getAutofillOptions() {
         return new CharSequence[0];
     }
+
 
     
 }
